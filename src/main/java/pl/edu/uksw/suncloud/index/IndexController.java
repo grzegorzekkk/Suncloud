71cafe7c12a6919d473ae5a30d767c17d3d16843
ziutekkk <grzesiek6_12@interia.pl>
2017-11-28 21:13:20 +0100

    Fix packaging structure.

--- src/main/java/pl/edu/uksw/suncloud/index/IndexController.java
+++ src/main/java/pl/edu/uksw/suncloud/index/IndexController.java
@@ -1,4 +1,4 @@
-package pl.edu.uksw.suncloud.suncloud.index;
+package pl.edu.uksw.suncloud.index;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
