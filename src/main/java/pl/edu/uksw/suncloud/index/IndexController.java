93c1c03511bacdb32ef7050451443305faa6e0d6
grzegorzekkk <grzesiek6_12@interia.pl>
2018-01-11 22:11:01 +0100

    Pass all measurements to index view and show example.

--- src/main/java/pl/edu/uksw/suncloud/index/IndexController.java
+++ src/main/java/pl/edu/uksw/suncloud/index/IndexController.java
@@ -1,13 +1,20 @@
 package pl.edu.uksw.suncloud.index;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.edu.uksw.suncloud.weather.MeasurementRepo;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    MeasurementRepo measurementRepo;
+
     @RequestMapping(value = "/")
-    public String index(){
+    public String index(Model model) {
+        model.addAttribute("msrmnts", measurementRepo.findAll());
         return "index";
     }
 }
